28bf5345bddf0184af20f902fb33e9a59479cc66
Antti Räisälä <antti.raisala@vismaconsulting.fi>
2024-02-29 05:20:37 +0200

    Minor change

--- src/libraries/LEDLights/LedLightCalculationTwoOperands.cpp
+++ src/libraries/LEDLights/LedLightCalculationTwoOperands.cpp
@@ -1,1 +1,13 @@
 #include "LedLightCalculationTwoOperands.h"
+
+LedLightCalculationTwoOperands::LedLightCalculationTwoOperands(LedLightCalculationTwoOperandsOperation operation, LedLightCalculationElement elementA, LedLightCalculationElement elementB) {
+  this->operation = operation;
+  this->elementA = elementA;
+  this->elementB = elementB;
+}
+
+LedLightCalculationValue LedLightCalculationTwoOperands::getValue(double currentTimeSeconds, double relativePhase, CalculationElementPhaseMapping calculationElementPhaseMapping) {
+
+
+  return this->ledLightCalculationValue;
+}
--- src/libraries/LEDLights/LedLightCalculationTwoOperands.h
+++ src/libraries/LEDLights/LedLightCalculationTwoOperands.h
@@ -1,7 +1,7 @@
 #ifndef LEDLIGHT_CALCULATION_TWO_OPERANDS_H
 #define LEDLIGHT_CALCULATION_TWO_OPERANDS_H
 
-#include <Arduino.h>
+#include "LedLightCalculationElement.h"
 
 enum class LedLightCalculationTwoOperandsOperation {
   ADD,
@@ -13,10 +13,16 @@ enum class LedLightCalculationTwoOperandsOperation {
   MIN
 };
 
-class LedLightCalculationTwoOperands {
+
+class LedLightCalculationTwoOperands : public LedLightCalculationElement {
 private:
+  LedLightCalculationTwoOperandsOperation operation;
+  LedLightCalculationElement elementA;
+  LedLightCalculationElement elementB;
 
 public:
+  LedLightCalculationTwoOperands(LedLightCalculationTwoOperandsOperation operation, LedLightCalculationElement elementA, LedLightCalculationElement elementB);
+  LedLightCalculationValue getValue(double currentTimeSeconds, double relativePhase, CalculationElementPhaseMapping calculationElementPhaseMapping);
 };
 
 #endif  // LEDLIGHT_CALCULATION_TWO_OPERANDS_H
