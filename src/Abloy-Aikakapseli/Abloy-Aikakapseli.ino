fdb50d66bcf1ea3cc81087ebf3b7c0da66b94e58
Antti Räisälä <antti.raisala@vismaconsulting.fi>
2024-02-29 16:28:25 +0200

    Minor change

--- src/Abloy-Aikakapseli/Abloy-Aikakapseli.ino
+++ src/Abloy-Aikakapseli/Abloy-Aikakapseli.ino
@@ -22,7 +22,6 @@ long nextMillisTo_printState = 0;
 
 #include "LedLightCalculationConstant.h"
 #include "LedLightCalculationSine.h"
-void setupLedColoringNodes(void);
 
 // Tällä havaitaan asiakkaan lähestyminen
 #include "distance_sensor.h"
@@ -78,7 +77,7 @@ Timer *writeCountdownTimeToEepromTimer;
 
 
 
-
+#include "MVP_inits.h"
 
 
 // Tehdään ajastus, että tilat tarkastetaan 10ms välein
@@ -144,7 +143,7 @@ void setup() {
     DEBUG_PRINTLN("aikakapseliEeprom.read() == false");
   }
 
-  setupLedColoringNodes();
+  coloring_DistaneStateFAR();
 
   Serial.println("Setup done.");
 }
@@ -259,55 +258,3 @@ void checkForStateChanges() {
   }
 }
 
-
-void setupLedColoringNodes(void) {
-  CalculationElementPhaseMapping cepmSame;
-  cepmSame.startPhase = 0.0;
-  cepmSame.endPhase = 0.0;
-
-  LedLightCalculationConstant *constantGreen = new LedLightCalculationConstant(0.0, 255.0, 0.0);
-  ledLights.setCalculations(0, constantGreen, cepmSame);
-  //
-  ledLights.setCalculations(5, constantGreen, cepmSame);
-
-  CalculationElementPhaseMapping cepmSine0;
-  cepmSine0.startPhase = 3.0 * 3.14159265359 / 60.0 * 0.0;
-  cepmSine0.endPhase = 3.0 * 3.14159265359 / 60.0 * 9.0;
-  CalculationElementPhaseMapping cepmSine1;
-  cepmSine1.startPhase = 3.0 * 3.14159265359 / 60.0 * 10.0;
-  cepmSine1.endPhase = 3.0 * 3.14159265359 / 60.0 * 19.0;
-  CalculationElementPhaseMapping cepmSine2;
-  cepmSine2.startPhase = 3.0 * 3.14159265359 / 60.0 * 20.0;
-  cepmSine2.endPhase = 3.0 * 3.14159265359 / 60.0 * 29.0;
-  CalculationElementPhaseMapping cepmSine3;
-  cepmSine3.startPhase = 3.0 * 3.14159265359 / 60.0 * 30.0;
-  cepmSine3.endPhase = 3.0 * 3.14159265359 / 60.0 * 39.0;
-  CalculationElementPhaseMapping cepmSine4;
-  cepmSine4.startPhase = 3.0 * 3.14159265359 / 60.0 * 40.0;
-  cepmSine4.endPhase = 3.0 * 3.14159265359 / 60.0 * 49.0;
-  CalculationElementPhaseMapping cepmSine5;
-  cepmSine5.startPhase = 3.0 * 3.14159265359 / 60.0 * 50.0;
-  cepmSine5.endPhase = 3.0 * 3.14159265359 / 60.0 * 59.0;
-
-  LedLightCalculationSine *ledLightCalculationSine = new LedLightCalculationSine(1.0, 1.0 / 2.0, 0.4, 0.6);
-
-  ledLights.setCalculations(0, ledLightCalculationSine, cepmSine0);
-  ledLights.setCalculations(1, ledLightCalculationSine, cepmSine1);
-  ledLights.setCalculations(2, ledLightCalculationSine, cepmSine2);
-  ledLights.setCalculations(3, ledLightCalculationSine, cepmSine3);
-  ledLights.setCalculations(4, ledLightCalculationSine, cepmSine4);
-  ledLights.setCalculations(5, ledLightCalculationSine, cepmSine5);
-
-
-  CalculationElementPhaseMapping cepmNoteSlot0;
-  cepmNoteSlot0.startPhase = 3.0 * 3.14159265359 / 19.0 * 0.0;
-  cepmNoteSlot0.endPhase = 3.0 * 3.14159265359 / 19.0 * 9.0;
-  CalculationElementPhaseMapping cepmNoteSlot1;
-  cepmNoteSlot1.startPhase = 3.0 * 3.14159265359 / 19.0 * 10.0;
-  cepmNoteSlot1.endPhase = 3.0 * 3.14159265359 / 19.0 * 19.0;
-  //
-  LedLightCalculationSine *noteSlotCalculations = new LedLightCalculationSine(0.0, 1.0 / 4.92348572983475, 0.4, 0.6);
-  //
-  ledLights.setCalculations(6, noteSlotCalculations, cepmNoteSlot0);
-  ledLights.setCalculations(7, noteSlotCalculations, cepmNoteSlot1);
-}
