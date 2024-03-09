adefbd81b6bf6bc41badc1f47f430c99358541e2
Antti Räisälä <antti.raisala@vismaconsulting.fi>
2024-03-09 21:28:29 +0200

    Minor change

--- src/Abloy-Aikakapseli/InitLedPatterns.cpp
+++ src/Abloy-Aikakapseli/InitLedPatterns.cpp
@@ -23,7 +23,7 @@ void initPatterns(void) {
   branchByDistance->debugPrint();
   delay(1000);
 
-/*
+  /*
   stateManager.setDistanceState(750, StateManager::DistanceState::FAR);
   stateManager.setNoteState(750, StateManager::NoteState::NO_NOTE);
   ledLights.loopSetColors(750);
@@ -36,6 +36,54 @@ void initPatterns(void) {
   cd->setInput0Element(new CalculationElementLink(llc_colorWhite));
   cd->setInput1Element(new CalculationElementLink(llc_colorGreen));
   ledLights.setCalculationElementLink(1, new CalculationElementLink(cd));
+
+
+  /************************************************************/
+  /* Note slot - begins */
+
+  /* NO_NOTE - begins */
+  LedLightCalculationConstant *slotWhite = new LedLightCalculationConstant(255.0, 255.0, 255.0);
+  LedLightCalculationConstant *slotFive = new LedLightCalculationConstant(5.0);
+  LedLightCalculationSine *slotNoNoteSinRotate = (new LedLightCalculationSine(0.0, 0.5, 0.2, 0.6))->setCalculationElementPhaseMapping(0.0, 2.0 * 3.14159265359 * 2.0);
+  LedLightCalculationTwoOperands *slotPowerFive = new LedLightCalculationTwoOperands(LedLightCalculationTwoOperandsOperation::POW, new CalculationElementLink(slotNoNoteSinRotate), new CalculationElementLink(slotFive));
+  LedLightCalculationTwoOperands *slotNoNoteMultiplication = new LedLightCalculationTwoOperands(LedLightCalculationTwoOperandsOperation::MULTIPLY, new CalculationElementLink(slotWhite), new CalculationElementLink(slotPowerFive));
+  int slotLedCount = 19;
+  double slotEndPhase = 1.0;
+  /*
+  ledLights.setCalculationElementLink(2, new CalculationElementLink(slotNoNoteMultiplication, slotEndPhase / slotLedCount * 00.0, slotEndPhase / slotLedCount * 09.0));
+  ledLights.setCalculationElementLink(3, new CalculationElementLink(slotNoNoteMultiplication, slotEndPhase / slotLedCount * 10.0, slotEndPhase / slotLedCount * 19.0));
+  */
+  /* NO_NOTE - ends */
+
+  /* INSERTING- begins */
+  LedLightCalculationConstant *slotLightGreen = new LedLightCalculationConstant(75.0, 255.0, 75.0);
+  //ledLights.setCalculationElementLink(2, new CalculationElementLink(slotLightGreen));
+  /* INSERTING- ends */
+
+  /* DROPPED - begins */
+  LedLightCalculationConstant *slotDroppedWhite = new LedLightCalculationConstant(255.0, 255.0, 255.0);
+  LedLightCalculationConstant *slotDroppedGreen = new LedLightCalculationConstant(0.0, 255.0, 0.0);
+  StateChangePulse *slotDroppedScp = (new StateChangePulse())->setState((byte)StateManager::NoteState::DROPPED);
+  //
+  CrossDissolve *slotCD = new CrossDissolve();
+  slotCD->setControlElement(new CalculationElementLink(slotDroppedScp));
+  slotCD->setInput0Element(new CalculationElementLink(slotDroppedWhite));
+  slotCD->setInput1Element(new CalculationElementLink(slotDroppedGreen));
+  ledLights.setCalculationElementLink(2, new CalculationElementLink(slotCD));
+  /* DROPPED - ends */
+
+
+  /* Combined slot - begins */
+
+
+
+
+  /* Combined slot - ends */
+
+
+
+  /* Note slot - ends */
+  /************************************************************/
 
 
 
